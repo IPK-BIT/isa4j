d327c22751d4666f62bf313648be1dad73ed125c
Thyra <thyra@users.noreply.github.com>
2020-01-13 18:09:15 +0100

    Prepare for Unit Testing with JUnit5

---
+++ src/test/java/de/ipk_gatersleben/bit/bi/isa4j/InvestigationTest.java
@@ -0,0 +1,12 @@
+package de.ipk_gatersleben.bit.bi.isa4j;
+
+import org.junit.jupiter.api.Test;
+
+public class InvestigationTest {
+	
+    @Test
+    void justAnExample() {
+        System.out.println("This test method should be run");
+    }
+
+}
