1b5c0aeef07096eb8ad64f9abf9d8b31c0a0ab8c
Thyra <thyra@users.noreply.github.com>
2020-01-26 13:08:55 +0100

    Started implementing realistic performance test for isatools

--- src/test/resources/de/ipk_gatersleben/bit/bi/isa4j/performanceTests/isatools_performance_test.py
+++ src/test/resources/de/ipk_gatersleben/bit/bi/isa4j/performanceTests/isatools_performance_test.py
@@ -1,9 +1,12 @@
 import time
+import csv
+import os
+from datetime import datetime
 from memory_profiler import memory_usage
 from isatools.model import *
 from isatools import isatab
 
-def measure_simple(n_rows):
+def measure_minimal(n_rows):
     starting_time = time.process_time_ns()
     investigation = Investigation()
     investigation.identifier = "i1"
@@ -51,7 +54,7 @@ def measure_simple(n_rows):
     return time.process_time_ns() - starting_time
 
 
-def measure_medium(n_rows):
+def measure_reduced(n_rows):
     starting_time = time.process_time_ns()
     investigation = Investigation()
     investigation.identifier = "i1"
@@ -145,19 +148,140 @@ def measure_medium(n_rows):
     isatab.dump(investigation, "./")
     return time.process_time_ns() - starting_time
 
-import csv
-from datetime import datetime
+def measure_realistic(n_rows):
+    starting_time = time.process_time_ns()
+    investigation = Investigation()
+    investigation.identifier = "i1"
+
+    study = Study(filename="s_study.txt")
+    study.identifier = "s1"
+    investigation.studies.append(study)
+
+    # Ontologies
+    ontologies = {}
+    ontologies["NCBITaxon"] = OntologySource(name = "NCBITaxon", file="http://purl.obolibrary.org/obo/ncbitaxon", description="National Center for Biotechnology Information (NCBI) Organismal Classification")
+    ontologies["AGRO"] = OntologySource(name = "AGRO", file="http://purl.obolibrary.org/obo/agro/releases/2018-05-14/agro.owl", description="Agronomy Ontology", version="2018-05-14")
+    ontologies["UO"] = OntologySource(name = "UO", file="http://data.bioontology.org/ontologies/UO", description="Units of Measurement Ontology", version="38802")
+    investigation.ontology_source_references.extend(ontologies.values())
+
+    # Factors
+    fa_soil_cover = StudyFactor(name="Soil Cover")
+    fa_plant_movement = StudyFactor(name="Plant Movement")
+    study.factors.extend([fa_soil_cover, fa_plant_movement])
+
+    fav_covered = FactorValue(factor_name = fa_soil_cover, value="covered")
+    fav_uncovered = FactorValue(factor_name = fa_soil_cover, value="uncovered")
+    fav_rotating = FactorValue(factor_name = fa_plant_movement, value="rotating")
+    fav_stationary = FactorValue(factor_name = fa_plant_movement, value="stationary")
+
+    sample_collection_protocol = Protocol(name="sample collection")
+    study.protocols.append(sample_collection_protocol)
+
+    # Protocols
+    prot_phenotyping = Protocol(name="Phenotyping")
+    prot_growth      = Protocol(name="Growth")
+    prot_watering    = Protocol(name="Watering")
+    study.protocols.append(prot_phenotyping)
+    study.protocols.append(prot_growth)
+    study.protocols.append(prot_watering)
+
+    assay = Assay(filename="a_assay.txt")
+    study.assays.append(assay)
+
+    # Characteristics
+    common_characteristics = [
+        Characteristic(category=OntologyAnnotation(term="Organism"),value=OntologyAnnotation(term="Arabidopsis thaliana",term_source=ontologies["NCBITaxon"],term_accession="http://purl.obolibrary.org/obo/NCBITaxon_3702")),
+        Characteristic(category=OntologyAnnotation(term="Genus"),value=OntologyAnnotation(term="Arabidopsis",term_source=ontologies["NCBITaxon"],term_accession="http://purl.obolibrary.org/obo/NCBITaxon_3701")),
+        Characteristic(category=OntologyAnnotation(term="Species"),value=OntologyAnnotation(term="thaliana")),
+        Characteristic(category=OntologyAnnotation(term="Infraspecific Name"),value=OntologyAnnotation(term=" ")),
+        Characteristic(category=OntologyAnnotation(term="Biological Material Latitude"),value=OntologyAnnotation(term="51.827721")),
+        Characteristic(category=OntologyAnnotation(term="Biological Material Longitude"),value=OntologyAnnotation(term="11.27778")),
+        Characteristic(category=OntologyAnnotation(term="Material Source ID"),value=OntologyAnnotation(term="http://eurisco.ipk-gatersleben.de/apex/f?p=103:16:::NO::P16_EURISCO_ACC_ID:1668187")),
+        Characteristic(category=OntologyAnnotation(term="Seed Origin"),value=OntologyAnnotation(term="http://arabidopsis.info/StockInfo?NASC_id=22680")),
+        Characteristic(category=OntologyAnnotation(term="Growth Facility"),value=OntologyAnnotation(term="small LemnaTec phytochamber")),
+        Characteristic(category=OntologyAnnotation(term="Material Source Latitude"),value=OntologyAnnotation(term="51.827721")),
+        Characteristic(category=OntologyAnnotation(term="Material Source Longitude"),value=OntologyAnnotation(term="11.27778"))
+    ]
+
+    # Growth Parameters
+    growth_parameters = {} # Name => [Value, Value REF, Value Accession, Unit, Unit REF, Unit Accession]
+    with open("growth_parameters.csv") as gp:
+        r = csv.DictReader(gp, delimiter=';')
+        for row in r:
+            growth_parameters[row["Parameter name"]] = list(row.values())[1:len(row)]
+            prot_growth.parameters.append(ProtocolParameter(parameter_name=OntologyAnnotation(term=row["Parameter name"])))
+
+    growth_parameter_values = []
+    for param in prot_growth.parameters:
+        field_values = growth_parameters[param.parameter_name.term]
+        if field_values[3]:
+            if field_values[4]:
+                unit = OntologyAnnotation(term=field_values[3], term_accession=field_values[5], term_source = ontologies[field_values[4]])
+            else:
+                unit = OntologyAnnotation(term=field_values[3])
+            # If there is a unit, the value should be a number
+            value = float(field_values[0])
+        else:
+            unit = None
+            if field_values[1]:
+                value = OntologyAnnotation(term=field_values[0], term_accession=field_values[2], term_source = ontologies[field_values[1]])
+            else:
+                value = OntologyAnnotation(term=field_values[0])
+
+        growth_parameter_values.append(ParameterValue(
+            category=param,
+            value = value,
+            unit = unit
+        ))
+
+    # Write Study File
+    for i in range(0, n_rows):
+        source = Source(name='Plant_{}'.format(i))
+        sample = Sample(name="1135FA-{}".format(i))
+        study.samples.append(sample)
+        study.sources.append(source)
+
+        source.characteristics.extend(common_characteristics)
+
+        proc_growth = Process(executes_protocol=prot_growth)
+        proc_growth.parameter_values.extend(growth_parameter_values)
+        proc_growth.inputs.append(source)
+        proc_growth.outputs.append(sample)
+
+        study.process_sequence.append(proc_growth)
+
+
+    for i, sample in enumerate(study.samples):
+        pass
+
+#       sequencing_process = Process(executes_protocol=sequencing_protocol)
+#       sequencing_process.name = "assay-name-{}".format(i)
+#       sequencing_process.inputs.append(sample)
+
+#       datafile = DataFile(filename="sequenced-data-{}".format(i), label="Raw Data File", generated_from=[sample])
+#       sequencing_process.outputs.append(datafile)
+
+#       assay.samples.append(sample)
+#       assay.data_files.append(datafile)
+#       assay.process_sequence.append(sequencing_process)
+
+
+    isatab.dump(investigation, "./")
+    return time.process_time_ns() - starting_time
+
 with open('performance_test_results.csv', 'w') as csvfile:
     writer = csv.writer(csvfile, delimiter=',')
     writer.writerow(["platform", "row.complexity", "n.rows", "time.in.ns", "memory.usage.in.mb", "date.test.performed"])
-    for i in [1, 10, 100, 500, 1000, 5000, 10000]:
+    for i in []:
         for r in range(0, 5):
-            writer.writerow(["isatools", "simple", i, measure_simple(i), max(memory_usage((measure_simple, (i,)), interval=1)), str(datetime.now())])
-    for i in [1, 10, 100, 500, 1000, 5000, 10000]:
+            writer.writerow(["isatools", "minimal", i, measure_minimal(i), -1, str(datetime.now())])
+    for i in []:
         for r in range(0, 5):
-            writer.writerow(["isatools", "medium", i, measure_medium(i), max(memory_usage((measure_medium, (i,)), interval=1)), str(datetime.now())])
+            writer.writerow(["isatools", "reduced", i, measure_reduced(i), -1, str(datetime.now())])
+    for i in [1, 10]:
+        for r in range(0, 5):
+            writer.writerow(["isatools", "realistic", i, measure_realistic(i), max(memory_usage((measure_realistic, (i,)), interval=1)), str(datetime.now())])
 
-import os
 os.remove("i_investigation.txt")
-os.remove("s_study.txt")
-os.remove("a_assay.txt")
+#os.remove("s_study.txt")
+#os.remove("a_assay.txt")
