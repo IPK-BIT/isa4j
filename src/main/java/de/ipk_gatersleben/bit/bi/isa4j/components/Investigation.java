68caf91da2944327e7a682a024018b99f4f8adef
Daniel Arend <arendd@ipk-gatersleben.de>
2020-01-27 09:17:05 +0100

    add license header to all java classes

--- src/main/java/de/ipk_gatersleben/bit/bi/isa4j/components/Investigation.java
+++ src/main/java/de/ipk_gatersleben/bit/bi/isa4j/components/Investigation.java
@@ -1,7 +1,6 @@
 /**
- * Copyright (c) 2019 Leibniz Institute of Plant Genetics and Crop Plant Research (IPK), Gatersleben, Germany.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the GNU GPLv3 license (https://www.gnu.org/licenses/gpl-3.0.en.html)
+ * Copyright (c) 2020 Leibniz Institute of Plant Genetics and Crop Plant Research (IPK), Gatersleben, Germany.
+ * All rights reserved. This program and the accompanying materials are made available under the terms of the GNU GPLv3 license (https://www.gnu.org/licenses/gpl-3.0.en.html)
  *
  * Contributors:
  *      Leibniz Institute of Plant Genetics and Crop Plant Research (IPK), Gatersleben, Germany
--- src/main/java/de/ipk_gatersleben/bit/bi/isa4j/components/Material.java
+++ src/main/java/de/ipk_gatersleben/bit/bi/isa4j/components/Material.java
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2020 Leibniz Institute of Plant Genetics and Crop Plant Research (IPK), Gatersleben, Germany.
+ * All rights reserved. This program and the accompanying materials are made available under the terms of the GNU GPLv3 license (https://www.gnu.org/licenses/gpl-3.0.en.html)
+ *
+ * Contributors:
+ *      Leibniz Institute of Plant Genetics and Crop Plant Research (IPK), Gatersleben, Germany
+ */
 package de.ipk_gatersleben.bit.bi.isa4j.components;
 
 import java.util.HashMap;
